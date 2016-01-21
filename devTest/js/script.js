34dff0a5ccc9544e2589f4011c1c28393c82207d
Nick Hayward <nhayward@everspringpartners.com>
2016-01-20 21:24:58 -0600

    modifying the nav media queries and styling up the menu button a bit

--- devTest/js/script.js
+++ devTest/js/script.js
@@ -3,4 +3,11 @@ $(function() {
       $('#expandingMenu').slideToggle();
       $('#expandingMenu').css('display', 'flex');
     });
+
+	//collapses the expandable menu on screen sizes that don't use it
+    $(window).resize(function() {
+    	if ($(window).width() > 735) {
+    		$('#expandingMenu').slideUp();
+    	}
+    });
 });
